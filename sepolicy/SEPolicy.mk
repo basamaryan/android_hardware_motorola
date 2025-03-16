745d68c0e1dca4fc0358ea1461ff9d6e7c4d154c
basamaryan <basam.aryan@gmail.com>
2025-03-16 00:03:35 -0400

    [WIP] motorola: Initial sepolicy
    Taken from https://github.com/moto-common/android_device_motorola_common/tree/15/sepolicy and https://github.com/LineageOS/android_device_motorola_sm6225-common/tree/lineage-22.1/sepolicy
    
    Diff: 9a6ba0f#diff-e7d1f9213e79ed1c6c6defa28eb7c94b7602f2a1fced058fe28f61213f5c9067
    
    Change-Id: Ibeb20ccba58b19b0c114d30a18f2bc109429884e

---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+SEPOLICY_PATH := hardware/motorola/sepolicy
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    $(SEPOLICY_PATH)/common/system_ext/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    $(SEPOLICY_PATH)/common/system_ext/public
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(SEPOLICY_PATH)/common/vendor
+
+ifeq ($(BOARD_HAS_MTK_HARDWARE),true)
+  BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/mtk/vendor
+endif
+
+ifeq ($(PRODUCT_USES_QCOM_HARDWARE),true)
+  BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qti/vendor
+endif
